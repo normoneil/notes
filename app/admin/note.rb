85d901a3f8985675e635d508892151241df4d822
Raghu Betina <raghu@firstdraft.com>
2016-11-15 02:57:28 +0000

    Whitelist columns in Note for ActiveAdmin

--- app/admin/note.rb
+++ app/admin/note.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Note do
 
+ permit_params :body, :location, :device, :dtg
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
